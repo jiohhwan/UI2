85bb10a8b2608ac53e6a13ea13039f5cd4916809
jihwan <zerter@naver.com>
2024-07-04 00:41:34 +0900

    first upload

---
+++ controllers/productController.js
@@ -0,0 +1,87 @@
+const Product = require('../models/productModel')
+const asyncHandler = require('express-async-handler')
+
+
+//get all product
+const getProducts = asyncHandler(async(req, res) => {
+    try {
+        const products = await Product.find({});
+        const users = [
+            { name: "kim", email: "kim@abc.def"},
+            { name: "oh", email: "oh@aff.fff"},
+        ]
+        res.render("mainUI", { users: users, products: products});
+
+    } catch (error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+})
+
+//get a single product
+const getProduct = asyncHandler(async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findbyId(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+})
+
+//create new product
+const createProduct = asyncHandler(async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product); 
+
+    } catch (error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+})
+
+// update a product
+const updateProduct = asyncHandler(async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        //we cannot find any product in DB
+        if(!product){
+            return res.status(404).json({message : `cannot find any product with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+
+    } catch (error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+})
+
+
+const deleteProduct = asyncHandler(async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            res.status(404);
+            throw new Error(`cannot find any product with ID ${id}`);
+        }
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500);
+        throw new Error(error.message);
+    }
+})
+
+
+module.exports = {
+    getProducts,
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct
+}
--- server.js
+++ server.js
@@ -1,12 +1,53 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const productRoute = require('./routes/productRoute');
+const errorMiddleware = require('./middleware/errorMiddleware')
+var cors = require('cors')
+
+
+
 const app = express()
 
+const PORT = process.env.PORT || 3000
+const MONGO_URL = process.env.MONGO_URL
+const FRONTEND = process.env.FRONTEND
+
+var corsOptions = {
+    origin: FRONTEND, // env파일에서 프론트엔드의 ip주소를 입력 ex) http://127.0.0.1:5173
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  } 
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("./public"));
 
 //routes
-app.get('/', (req, res) => {
-    res.send('hello node api')
+
+app.use('/', productRoute);
+
+app.get('/blog', (req, res) => {
+    res.send('hello blog hi')
 })
 
-app.listen(3000, ()=> {
-    console.log('node api app is runing in port 3000');
+
+app.use(errorMiddleware);
+
+
+
+mongoose.set("strictQuery", false)
+mongoose.connect(MONGO_URL)
+.then(() => {
+    console.log('connected to mongoDB');
+    app.listen(PORT, ()=> {
+        console.log(`node api app is runing in port ${PORT}`);
+    });
+}).catch((error) => {
+    console.log(error);
 })
